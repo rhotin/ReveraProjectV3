8f28251d72816a708454bd845cdbbacfb0459b04
r47 <roman.hotin.rh@gmail.com>
2017-08-28 11:27:51 -0400

    Rev App Aug 28

--- app/src/main/java/com/ideaone/reveraprojectapp1/ReadFragment.java
+++ app/src/main/java/com/ideaone/reveraprojectapp1/ReadFragment.java
@@ -3,6 +3,7 @@ package com.ideaone.reveraprojectapp1;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,12 @@ public class ReadFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_read, container, false);
 
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new NewActivity().startNewActivity(view.getContext(), "com.google.android.youtube");
     }
 
 }
